576b741a4dcb8b659d10147288f6ffc143f501a9
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-09-26 01:14:07 +0200

    Added sentry

--- FuelStationsSpain/Constants/Config.swift
+++ FuelStationsSpain/Constants/Config.swift
@@ -2,6 +2,8 @@ import CoreLocation
 
 class Config {
     static let groupId = "group.com.jgeek00.FuelStationsSpain"
+    static let sentryDsn = "https://aad44fd1edd54259a39883101a4a1ec3@glitchtip.jgeek00.com/10"
+    
     static let defaultCoordinates = CLLocationCoordinate2D(latitude: 40.4166359, longitude: -3.7038101)
     static let minimumRefetchDistance = 10.0
 }
--- FuelStationsSpain/FuelStationsSpainApp.swift
+++ FuelStationsSpain/FuelStationsSpainApp.swift
@@ -1,8 +1,19 @@
 import SwiftUI
+import Sentry
 
 @main
 struct FuelStationsSpainApp: App {
     let persistenceController = PersistenceController.shared
+    
+    init() {
+        #if RELEASE
+        SentrySDK.start { options in
+            options.dsn = Config.sentryDsn
+            options.debug = false
+            options.tracesSampleRate = 0.5
+        }
+        #endif
+    }
 
     var body: some Scene {
         WindowGroup {
