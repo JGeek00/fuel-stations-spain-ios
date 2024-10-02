c62e795e24ba33f43f69032ee493778f3bcb6f32
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-10-02 16:00:01 +0200

    Fixed corner radius

--- FuelStationsSpain/Views/Map/StationDetailsSheet.swift
+++ FuelStationsSpain/Views/Map/StationDetailsSheet.swift
@@ -144,6 +144,7 @@ struct StationDetailsSheet: View {
                         }
                         StationDetailsComponents.PricesItem(station: station)
                             .customBackgroundWithMaterial()
+                            .clipShape(RoundedRectangle(cornerRadius: 8.0))
                         if let update = mapManager.data?.lastUpdated {
                             if let date = formatDate(update) {
                                 StationDetailsComponents.ListItem(
