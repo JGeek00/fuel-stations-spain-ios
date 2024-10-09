acb5711eb55355c77102e6646bf7f155c63a7293
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-10-09 19:40:44 +0200

    Calculate opening time onAppear to improve performance

--- FuelStationsSpain/Views/Map/MapView.swift
+++ FuelStationsSpain/Views/Map/MapView.swift
@@ -26,8 +26,6 @@ fileprivate struct MapComponent: View {
     @AppStorage(StorageKeys.hideStationsNotOpenPublic, store: UserDefaults.shared) private var hideStationsNotOpenPublic: Bool = Defaults.hideStationsNotOpenPublic
     @AppStorage(StorageKeys.favoriteFuel, store: UserDefaults.shared) private var favoriteFuel: Enums.FavoriteFuelType = Defaults.favoriteFuel
     @AppStorage(StorageKeys.hideStationsDontHaveFavoriteFuel, store: UserDefaults.shared) private var hideStationsDontHaveFavoriteFuel: Bool = Defaults.hideStationsDontHaveFavoriteFuel
-    @AppStorage(StorageKeys.closedStationsShowMethod, store: UserDefaults.shared) private var closedStationsShowMethod: Enums.ClosedStationsMode = Defaults.closedStationsShowMethod
-    @AppStorage(StorageKeys.showRedClockClosedStations, store: UserDefaults.shared) private var showRedClockClosedStations = Defaults.showRedClockClosedStations
 
     var body: some View {
         Map(position: $mapManager.position, bounds: MapCameraBounds(minimumDistance: 500, maximumDistance: 50000)) {
@@ -50,7 +48,8 @@ fileprivate struct MapComponent: View {
                 }()
                 ForEach(markers, id: \.id) { value in
                     Annotation(value.signage!, coordinate: CLLocationCoordinate2D(latitude: value.latitude!, longitude: value.longitude!)) {
-                        MapMarker(value)
+                        MapMarkerItem(value)
+                            .environmentObject(MapManager.shared)
                     }
                 }
             }
@@ -156,68 +155,6 @@ fileprivate struct MapComponent: View {
     }
     
     @ViewBuilder
-    private func MapMarker(_ value: FuelStation) -> some View {
-        let fuelPrice: Double? = FuelStation.getObjectProperty(station: value, propertyName: "\(favoriteFuel.rawValue)Price")
-        let formattedSchedule = value.openingHours != nil ? getStationSchedule(value.openingHours!) : nil
-        if !(formattedSchedule?.isCurrentlyOpen == false && closedStationsShowMethod == .hideCompletely) {
-            if favoriteFuel != .none, let fuelPrice = fuelPrice {
-                PriceMarker()
-                    .foregroundStyle(Color.background)
-                    .frame(width: 60, height: 34)
-                    .overlay(alignment: .center) {
-                        Text(verbatim: "\(formattedNumber(value: fuelPrice, digits: 3))€")
-                            .font(.system(size: 14))
-                            .fontWeight(.semibold)
-                            .padding(.bottom, 30*0.2)
-                    }
-                    .overlay(PriceMarker().stroke(Color.gray, lineWidth: 0.5))
-                    .overlay(alignment: .topTrailing, content: {
-                        if formattedSchedule?.isCurrentlyOpen == false && showRedClockClosedStations == true {
-                            RedClock()
-                        }
-                    })
-                    .opacity(formattedSchedule?.isCurrentlyOpen == false && closedStationsShowMethod == .showDimmed ? 0.5 : 1)
-                    .shadow(color: .black.opacity(0.3), radius: 5)
-                    .scaleEffect(value.id == mapManager.selectedStationAnimation?.id ? 1.5 : 1, anchor: .bottom)
-                    .animation(.bouncy(extraBounce: 0.2), value: mapManager.selectedStationAnimation?.id)
-                    .onTapGesture {
-                        mapManager.selectStation(station: value)
-                    }
-            }
-            else {
-                NormalMarker()
-                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.markerGradientStart, Color.markerGradientEnd]), startPoint: .top, endPoint: .bottom))
-                    .frame(width: 30, height: 30)
-                    .overlay(alignment: .topTrailing, content: {
-                        if formattedSchedule?.isCurrentlyOpen == false && showRedClockClosedStations == true {
-                            RedClock()
-                        }
-                    })
-                    .opacity(formattedSchedule?.isCurrentlyOpen == false && closedStationsShowMethod == .showDimmed ? 0.5 : 1)
-                    .scaleEffect(value.id == mapManager.selectedStationAnimation?.id ? 1.5 : 1, anchor: .bottom)
-                    .animation(.bouncy(extraBounce: 0.2), value: mapManager.selectedStationAnimation?.id)
-                    .onTapGesture {
-                        mapManager.selectStation(station: value)
-                    }
-            }
-        }
-    }
-    
-    @ViewBuilder
-    private func RedClock() -> some View {
-        Circle()
-            .offset(x: 6, y: -6)
-            .frame(width: 15, height: 15)
-            .foregroundStyle(Color.white)
-            .overlay(alignment: .center) {
-                Image(systemName: "clock.fill")
-                    .offset(x: 6, y: -6)
-                    .foregroundStyle(Color.red)
-                    .font(.system(size: 14))
-            }
-    }
-    
-    @ViewBuilder
     private func MapOverlay() -> some View {
         GeometryReader(content: { geometry in
             Group {
@@ -286,4 +223,86 @@ fileprivate struct MapComponent: View {
             }
         })
     }
+}
+
+fileprivate struct MapMarkerItem: View {
+    var value: FuelStation
+    
+    init(_ value: FuelStation) {
+        self.value = value
+    }
+    
+    @EnvironmentObject private var mapManager: MapManager
+    
+    @AppStorage(StorageKeys.closedStationsShowMethod, store: UserDefaults.shared) private var closedStationsShowMethod: Enums.ClosedStationsMode = Defaults.closedStationsShowMethod
+    @AppStorage(StorageKeys.showRedClockClosedStations, store: UserDefaults.shared) private var showRedClockClosedStations = Defaults.showRedClockClosedStations
+    @AppStorage(StorageKeys.favoriteFuel, store: UserDefaults.shared) private var favoriteFuel: Enums.FavoriteFuelType = Defaults.favoriteFuel
+    
+    @State private var formattedSchedule: OpeningSchedule?
+    
+    var body: some View {
+        let fuelPrice: Double? = FuelStation.getObjectProperty(station: value, propertyName: "\(favoriteFuel.rawValue)Price")
+        Group {
+            if !(formattedSchedule?.isCurrentlyOpen == false && closedStationsShowMethod == .hideCompletely) {
+                if favoriteFuel != .none, let fuelPrice = fuelPrice {
+                    PriceMarker()
+                        .foregroundStyle(Color.background)
+                        .frame(width: 60, height: 34)
+                        .overlay(alignment: .center) {
+                            Text(verbatim: "\(formattedNumber(value: fuelPrice, digits: 3))€")
+                                .font(.system(size: 14))
+                                .fontWeight(.semibold)
+                                .padding(.bottom, 30*0.2)
+                        }
+                        .overlay(PriceMarker().stroke(Color.gray, lineWidth: 0.5))
+                        .overlay(alignment: .topTrailing, content: {
+                            if formattedSchedule?.isCurrentlyOpen == false && showRedClockClosedStations == true {
+                                RedClock()
+                            }
+                        })
+                        .opacity(formattedSchedule?.isCurrentlyOpen == false && closedStationsShowMethod == .showDimmed ? 0.5 : 1)
+                        .shadow(color: .black.opacity(0.3), radius: 5)
+                        .scaleEffect(value.id == mapManager.selectedStationAnimation?.id ? 1.5 : 1, anchor: .bottom)
+                        .animation(.bouncy(extraBounce: 0.2), value: mapManager.selectedStationAnimation?.id)
+                        .onTapGesture {
+                            mapManager.selectStation(station: value)
+                        }
+                }
+                else {
+                    NormalMarker()
+                        .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.markerGradientStart, Color.markerGradientEnd]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 30, height: 30)
+                        .overlay(alignment: .topTrailing, content: {
+                            if formattedSchedule?.isCurrentlyOpen == false && showRedClockClosedStations == true {
+                                RedClock()
+                            }
+                        })
+                        .opacity(formattedSchedule?.isCurrentlyOpen == false && closedStationsShowMethod == .showDimmed ? 0.5 : 1)
+                        .scaleEffect(value.id == mapManager.selectedStationAnimation?.id ? 1.5 : 1, anchor: .bottom)
+                        .animation(.bouncy(extraBounce: 0.2), value: mapManager.selectedStationAnimation?.id)
+                        .onTapGesture {
+                            mapManager.selectStation(station: value)
+                        }
+                }
+            }
+        }
+        .onAppear {
+            formattedSchedule = value.openingHours != nil ? getStationSchedule(value.openingHours!) : nil
+        }
+    }
+    
+    @ViewBuilder
+    private func RedClock() -> some View {
+        Circle()
+            .offset(x: 6, y: -6)
+            .frame(width: 15, height: 15)
+            .foregroundStyle(Color.white)
+            .overlay(alignment: .center) {
+                Image(systemName: "clock.fill")
+                    .offset(x: 6, y: -6)
+                    .foregroundStyle(Color.red)
+                    .font(.system(size: 14))
+            }
+    }
+    
 }
