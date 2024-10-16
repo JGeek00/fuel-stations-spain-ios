098af61c89886c4ca2aa1c5541f253b09a89332a
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-10-16 17:01:17 +0200

    Some improvements

--- FuelStationsSpain/Views/Favorites/FavoritesView.swift
+++ FuelStationsSpain/Views/Favorites/FavoritesView.swift
@@ -29,66 +29,19 @@ struct FavoritesView: View {
             NavigationSplitView(columnVisibility: $columnVisibility) {
                 Group {
                     if favoritesProvider.favorites.isEmpty {
-                        ContentUnavailableView("No favorite stations", systemImage: "list.bullet", description: Text("Mark some service stations as favorites to see them here."))                }
+                        ContentUnavailableView("No favorite stations", systemImage: "list.bullet", description: Text("Mark some service stations as favorites to see them here."))
+                    }
                     else {
                         if favoritesListViewModel.loading == true {
                             ProgressView()
                                 .transition(.opacity)
                         }
-                        else if favoritesListViewModel.data != nil {
-                            if favoritesListViewModel.data!.results!.isEmpty {
-                                ContentUnavailableView("No favorite stations", systemImage: "list.bullet", description: Text("Mark some service stations as favorites to see them here."))
-                            }
-                            else {
-                                let data = addDistancesToStations(stations: favoritesListViewModel.data!.results!, lastLocation: location)
-                                let sorted = sortStations(stations: data, sortingMethod: selectedSorting)
-                                let filtered = searchText != "" ? sorted.filter() { $0.signage!.lowercased().contains(searchText.lowercased()) } : sorted
-                                Group {
-                                    if listHasContent == false {
-                                        ContentUnavailableView("No results", systemImage: "magnifyingglass", description: Text("Change the inputted search term."))
-                                            .transition(.opacity)
-                                    }
-                                    else {
-                                        List(selection: $selectedStation) {
-                                            Section {
-                                                ForEach(filtered, id: \.self) { item in
-                                                    StationListEntry(station: item, sortingMethod: selectedSorting, hideFavoriteSymbol: true)
-                                                }
-                                            } header: {
-                                                Text(sortingText(sortingMethod: selectedSorting))
-                                                    .fontWeight(.semibold)
-                                                    .padding(.bottom, 12)
-                                                    .padding(.leading, horizontalSizeClass == .regular ? 0 : -12)
-                                                    .padding(.top, horizontalSizeClass == .regular ? 0 : -12)
-                                                    .textCase(nil)
-                                                    .fontSize(14)
-                                            }
-                                        }
-                                        .animation(.default, value: filtered)
-                                        .transition(.opacity)
-                                        .listStyle(.insetGrouped)
-                                    }
-                                }
-                                .onChange(of: filtered) {
-                                    withAnimation(.default) {
-                                        if filtered.isEmpty {
-                                            listHasContent = false
-                                        }
-                                        else {
-                                            listHasContent = true
-                                        }
-                                    }
-                                }
-                                .searchable(text: $searchText, prompt: "Search service station by name")
-                                .toolbar {
-                                    ToolbarItem(placement: .topBarTrailing) {
-                                        SortingPicker(selectedSorting: $selectedSorting)
-                                    }
-                                }
-                            }
+                        else if favoritesListViewModel.error != nil {
+                            ContentUnavailableView("Cannot load stations", systemImage: "exclamationmark.circle.fill", description: Text("An error occured when loading the list of service stations. Try again later."))
+                                .transition(.opacity)
                         }
                         else {
-                            ContentUnavailableView("Cannot load favorites", systemImage: "exclamationmark.circle.fill", description: Text("An error occured when loading the favorites information. Try again later."))
+                            Content()
                                 .transition(.opacity)
                         }
                     }
@@ -116,6 +69,72 @@ struct FavoritesView: View {
             .onChange(of: proxy.size.width) {
                 width = proxy.size.width
             }
+        }
+    }
+    
+    @ViewBuilder private func Content() -> some View {
+        if let data = favoritesListViewModel.data?.results {
+            if data.isEmpty {
+                ContentUnavailableView("No stations", systemImage: "fuelpump.slash.fill", description: Text("This municipality has no service stations."))
+                    .transition(.opacity)
+            }
+            else {
+                let dataWithDistance = addDistancesToStations(stations: data, lastLocation: location)
+                let sorted = sortStations(stations: dataWithDistance, sortingMethod: selectedSorting)
+                let filtered = searchText != "" ? sorted.filter() { $0.signage!.lowercased().contains(searchText.lowercased()) } : sorted
+                Group {
+                    if listHasContent == false {
+                        ContentUnavailableView("No results", systemImage: "magnifyingglass", description: Text("Change the inputted search term."))
+                            .transition(.opacity)
+                    }
+                    else {
+                        List(selection: $selectedStation) {
+                            Section {
+                                ForEach(filtered, id: \.self) { item in
+                                    StationListEntry(station: item, sortingMethod: selectedSorting)
+                                }
+                            } header: {
+                                HStack {
+                                    if horizontalSizeClass == .regular {
+                                        Spacer()
+                                    }
+                                    Text(sortingText(sortingMethod: selectedSorting))
+                                        .fontWeight(.semibold)
+                                        .multilineTextAlignment(horizontalSizeClass == .regular ? .center : .leading)
+                                        .padding(.bottom, 12)
+                                        .padding(.leading, horizontalSizeClass == .regular ? 0 : -12)
+                                        .padding(.top, horizontalSizeClass == .regular ? 0 : -12)
+                                        .textCase(nil)
+                                        .fontSize(14)
+                                    Spacer()
+                                }
+                            }
+                        }
+                        .animation(.default, value: filtered)
+                        .transition(.opacity)
+                        .listStyle(.insetGrouped)
+                    }
+                }
+                .onChange(of: filtered) {
+                    withAnimation(.default) {
+                        if filtered.isEmpty {
+                            listHasContent = false
+                        }
+                        else {
+                            listHasContent = true
+                        }
+                    }
+                }
+                .searchable(text: $searchText, prompt: "Search service station by name")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        SortingPicker(selectedSorting: $selectedSorting)
+                    }
+                }
+            }
+        }
+        else {
+            ContentUnavailableView("Cannot load favorites", systemImage: "exclamationmark.circle.fill", description: Text("An error occured when loading the favorites information. Try again later."))
         }
     }
 }
--- FuelStationsSpain/Views/Search/StationsList.swift
+++ FuelStationsSpain/Views/Search/StationsList.swift
@@ -20,71 +20,11 @@ struct SearchStationsList: View {
                     }
                     else if searchViewModel.stationsError == true {
                         ContentUnavailableView("Cannot load stations", systemImage: "exclamationmark.circle.fill", description: Text("An error occured when loading the list of service stations. Try again later."))
+                            .transition(.opacity)
                     }
                     else {
-                        if let data = searchViewModel.stationsData?.results {
-                            let data = addDistancesToStations(stations: data, lastLocation: searchViewModel.location)
-                            if data.isEmpty {
-                                ContentUnavailableView("No stations", systemImage: "fuelpump.slash.fill", description: Text("This municipality has no service stations."))
-                                    .transition(.opacity)
-                            }
-                            else {
-                                let sorted = sortStations(stations: data, sortingMethod: searchViewModel.stationsSelectedSorting)
-                                let filtered = searchViewModel.stationsSearchText != "" ? sorted.filter() { $0.signage!.lowercased().contains(searchViewModel.stationsSearchText.lowercased()) } : sorted
-                                Group {
-                                    if searchViewModel.stationsListHasContent == false {
-                                        ContentUnavailableView("No results", systemImage: "magnifyingglass", description: Text("Change the inputted search term."))
-                                            .transition(.opacity)
-                                    }
-                                    else {
-                                        List(selection: $searchViewModel.selectedStation) {
-                                            Section {
-                                                ForEach(filtered, id: \.self) { item in
-                                                    StationListEntry(station: item, sortingMethod: searchViewModel.stationsSelectedSorting)
-                                                }
-                                            } header: {
-                                                HStack {
-                                                    if horizontalSizeClass == .regular {
-                                                        Spacer()
-                                                    }
-                                                    Text(sortingText(sortingMethod: searchViewModel.stationsSelectedSorting))
-                                                        .fontWeight(.semibold)
-                                                        .multilineTextAlignment(horizontalSizeClass == .regular ? .center : .leading)
-                                                        .padding(.bottom, 12)
-                                                        .padding(.leading, horizontalSizeClass == .regular ? 0 : -12)
-                                                        .padding(.top, horizontalSizeClass == .regular ? 0 : -12)
-                                                        .textCase(nil)
-                                                        .fontSize(14)
-                                                    Spacer()
-                                                }
-                                            }
-                                        }
-                                        .animation(.default, value: filtered)
-                                        .transition(.opacity)
-                                        .listStyle(.insetGrouped)
-                                    }
-                                }
-                                .onChange(of: filtered) {
-                                    withAnimation(.default) {
-                                        if filtered.isEmpty {
-                                            searchViewModel.stationsListHasContent = false
-                                        }
-                                        else {
-                                            searchViewModel.stationsListHasContent = true
-                                        }
-                                    }
-                                }
-                                .searchable(text: $searchViewModel.stationsSearchText, prompt: "Search service station by name")
-                                .toolbar {
-                                    ToolbarItem(placement: .topBarTrailing) {
-                                        SortingPicker(selectedSorting: $searchViewModel.stationsSelectedSorting)
-                                    }
-                                }
-                            }
-                        }
-                        else {
-                            ContentUnavailableView("Cannot load stations", systemImage: "exclamationmark.circle.fill", description: Text("An error occured when loading the list of service stations. Try again later."))
-                        }
+                        Content()
+                            .transition(.opacity)
                     }
                 }
                 .navigationTitle(selectedMunicipality.Municipio?.capitalized ?? String(localized: "Municipality"))
@@ -93,6 +33,74 @@ struct SearchStationsList: View {
             else {
                 ContentUnavailableView("Select a municipality", systemImage: "building.columns.fill", description: Text("Select a municipality to see it's service stations."))
             }
+        }
+    }
+    
+    @ViewBuilder private func Content() -> some View {
+        @Bindable var searchViewModel = searchViewModel
+        
+        if let data = searchViewModel.stationsData?.results {
+            if data.isEmpty {
+                ContentUnavailableView("No stations", systemImage: "fuelpump.slash.fill", description: Text("This municipality has no service stations."))
+                    .transition(.opacity)
+            }
+            else {
+                let dataWithDistance = addDistancesToStations(stations: data, lastLocation: searchViewModel.location)
+                let sorted = sortStations(stations: dataWithDistance, sortingMethod: searchViewModel.stationsSelectedSorting)
+                let filtered = searchViewModel.stationsSearchText != "" ? sorted.filter() { $0.signage!.lowercased().contains(searchViewModel.stationsSearchText.lowercased()) } : sorted
+                Group {
+                    if searchViewModel.stationsListHasContent == false {
+                        ContentUnavailableView("No results", systemImage: "magnifyingglass", description: Text("Change the inputted search term."))
+                            .transition(.opacity)
+                    }
+                    else {
+                        List(selection: $searchViewModel.selectedStation) {
+                            Section {
+                                ForEach(filtered, id: \.self) { item in
+                                    StationListEntry(station: item, sortingMethod: searchViewModel.stationsSelectedSorting)
+                                }
+                            } header: {
+                                HStack {
+                                    if horizontalSizeClass == .regular {
+                                        Spacer()
+                                    }
+                                    Text(sortingText(sortingMethod: searchViewModel.stationsSelectedSorting))
+                                        .fontWeight(.semibold)
+                                        .multilineTextAlignment(horizontalSizeClass == .regular ? .center : .leading)
+                                        .padding(.bottom, 12)
+                                        .padding(.leading, horizontalSizeClass == .regular ? 0 : -12)
+                                        .padding(.top, horizontalSizeClass == .regular ? 0 : -12)
+                                        .textCase(nil)
+                                        .fontSize(14)
+                                    Spacer()
+                                }
+                            }
+                        }
+                        .animation(.default, value: filtered)
+                        .transition(.opacity)
+                        .listStyle(.insetGrouped)
+                    }
+                }
+                .onChange(of: filtered) {
+                    withAnimation(.default) {
+                        if filtered.isEmpty {
+                            searchViewModel.stationsListHasContent = false
+                        }
+                        else {
+                            searchViewModel.stationsListHasContent = true
+                        }
+                    }
+                }
+                .searchable(text: $searchViewModel.stationsSearchText, prompt: "Search service station by name")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        SortingPicker(selectedSorting: $searchViewModel.stationsSelectedSorting)
+                    }
+                }
+            }
+        }
+        else {
+            ContentUnavailableView("Cannot load stations", systemImage: "exclamationmark.circle.fill", description: Text("An error occured when loading the list of service stations. Try again later."))
         }
     }
 }
